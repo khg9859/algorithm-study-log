ee8cb13bee6d1d058ba0e1d88a1f958f6ac40712
Hong Geun Kim <khg9859@gmail.com>
2026-07-15 15:28:01 +0900

    [level 0] Title: 더 크게 합치기, Time: 0.00 ms, Memory: 11.7 MB -BaekjoonHub

---
+++ programmers/0/181939-더 크게 합치기/더 크게 합치기.py
@@ -0,0 +1,14 @@
+# input()만 문자열로 반환
+
+def solution(a, b):
+    answer = 0
+    
+    ab = int(str(a) + str(b))
+    ba = int(str(b) + str(a))
+    
+    if ab > ba:
+        answer = ab
+    else:
+        answer = ba
+
+    return answer
