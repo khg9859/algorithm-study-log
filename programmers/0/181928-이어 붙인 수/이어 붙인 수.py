51a3725b0568f34887d1da9a76cc3ad0bad85a11
Ian Kim <khg9859@gmail.com>
2026-08-06 12:15:04 +0900

    [level 0] Title: 이어 붙인 수, Time: 0.01 ms, Memory: 11.4 MB -BaekjoonHub

---
+++ programmers/0/181928-이어 붙인 수/이어 붙인 수.py
@@ -0,0 +1,12 @@
+def solution(num_list):
+    odd = ""
+    even = ""
+    
+    
+    for n in num_list:
+        if n % 2 == 0:
+            even += str(n)
+        else:
+            odd += str(n)
+            
+    return int(odd) + int(even)
